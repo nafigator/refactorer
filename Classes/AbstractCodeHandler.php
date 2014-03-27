c22e1856834dcee17b4a06e6bc381f913bea8467
Alexander Yancharuk <alex@itvault.info>
2014-03-27 17:49:43 +0400

    Method for checking rules moved to AbstractCodeHandler class

--- Classes/AbstractCodeHandler.php
+++ Classes/AbstractCodeHandler.php
@@ -28,14 +28,6 @@ abstract class AbstractCodeHandler
 	protected $rules;
 
 	/**
-	 * Checking token for compliance with the rules
-	 *
-	 * @param iToken $token
-	 * @return bool
-	 */
-	abstract public function checkRules(iToken $token);
-
-	/**
 	 * Convert tokens into Expression
 	 *
 	 * @param array $tokens
@@ -67,5 +59,62 @@ abstract class AbstractCodeHandler
 	public function setRules(ContextRules $rules)
 	{
 		$this->rules = $rules;
+	}
+
+	/**
+	 * Checking token for compliance with the rules
+	 *
+	 * @param iToken $token
+	 * @return bool
+	 */
+	public function checkRules(iToken $token)
+	{
+		if ($this->rules->isEmpty()) {
+			$this->rules->reset();
+			return true;
+		}
+
+		$check = (null === $this->final)
+			? $this->rules->current()
+			: $this->final;
+
+		if ('*' === $check->getContent()) {
+			$this->final = $this->rules->current();
+			$this->context[] = $token;
+			return false;
+		}
+
+		// Финальные токены не определены
+		if (null !== $this->final) {
+			// Финальные токены определены - добавляем токен в контекс
+			$this->context[] = $token;
+			// если ещё и совпадает контент - обнуляем финальный токен
+			if ($token->getContent() === $check->getContent()) {
+				$this->final = null;
+			}
+
+			return false;
+		}
+
+		// Если токен не соответствует токену из очереди делаем ресет
+		if ($token->getIndex() !== $check->getIndex()) {
+			$this->context = [];
+			$this->rules->reset();
+			return false;
+		}
+
+		// Если токен без контента, то сразу добавляем в контекст
+		if (null === $check->getContent()) {
+			$this->context[] = $token;
+			return false;
+		}
+
+		// Если у токена есть контент и он соответствует правилу, добавляем в контекст
+		if ($token->getContent() === $check->getContent()) {
+			$this->context[] = $token;
+			return false;
+		}
+
+		return false;
 	}
 }
